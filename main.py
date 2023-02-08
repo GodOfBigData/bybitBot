00fd45ac3d35dedfc062cf6302578d01bf82cb7a
GodOfBigData <stincs369@mail.ru>
2023-02-09 01:37:30 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from  strategies.levelsSrategy import runStretagy
 from configs.config import api_key, api_secret, proxy, currency, interval
+from time import sleep
 
 
 if __name__ == "__main__":
+    sleep(30)
     runStretagy(api_key, api_secret, currency, proxy, interval)
     
